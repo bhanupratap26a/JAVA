03534e6935bd0bdb9b480581a64f9ee40cc44aba
Bhanupratap Naik <bhanupratapnaik26@gmail.com>
2025-01-11 23:38:39 +0530

    updated

---
+++ add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class add {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any two Num");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int res = a + b;
+
+        System.out.println("The Sum is : "+res);
+    }
+}
